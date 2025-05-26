25344797bca0bf9f319a967a444fbe3e34890228
sakshisingh0101 <sakshiaingh0101>
2025-05-27 02:15:04 +0530

    add env local var

--- Project_Frontend/BlogPost_Frontend/src/main.jsx
+++ Project_Frontend/BlogPost_Frontend/src/main.jsx
@@ -17,6 +17,20 @@ import { RouterProvider } from 'react-router-dom'
 import { store } from './authStore/store.js'
 import { useDispatch } from 'react-redux'
 import { addUserData,login_status_toggle } from './authStore/authSlice.js'
+
+
+if (import.meta.env.VITE_BACKEND_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+} else if (import.meta.env.DEV) { // If in development mode (locally) and VITE_BACKEND_URL is not set
+  // This means you are relying on the vite.config.js proxy for local dev
+  // Setting base URL to '/' ensures that '/api/v1/...' correctly hits the proxy
+  axios.defaults.baseURL = '/';
+}
+// For production, VERCEL_BACKEND_URL should *always* be set.
+
+// Set withCredentials globally if all your requests need it
+axios.defaults.withCredentials = true;
+
 const router=createBrowserRouter(
 
   [ {

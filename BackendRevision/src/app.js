16be533fb0811ac6544f521065b71f772be71224
sakshisingh0101 <sakshiaingh0101>
2025-05-28 00:52:17 +0530

    cors and env reset for deployment purpose

--- BackendRevision/src/app.js
+++ BackendRevision/src/app.js
@@ -3,11 +3,24 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser'
 
 const app=express();
+const allowedOriginsString = process.env.CORS_ORIGIN;
+const allowedOrigins = allowedOriginsString ? allowedOriginsString.split(',').map(s => s.trim()) : [];
+
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,
+//     credentials:true
+// }))
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json(
     {
         limit:"16kb"
